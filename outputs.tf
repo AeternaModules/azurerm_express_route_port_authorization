659961503e4bf95c8fb57a4b8c3a796d8777ff42
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:53:34 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "express_route_port_authorizations" {
+  description = "All express_route_port_authorization resources"
+  value       = azurerm_express_route_port_authorization.express_route_port_authorizations
+}
+
